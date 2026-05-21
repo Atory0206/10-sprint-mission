5ef0724784e848633d301fee7f6c2cc8cb24ec69
안승리 <hauall@naver.com>
2026-05-21 11:02:54 +0900

    sprint 9 피드백 수정

--- src/main/java/com/sprint/mission/discodeit/auth/LoginFailureHandler.java
+++ src/main/java/com/sprint/mission/discodeit/auth/LoginFailureHandler.java
@@ -1,6 +1,7 @@
 package com.sprint.mission.discodeit.auth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sprint.mission.discodeit.common.exception.ErrorResponse;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -19,10 +20,20 @@ public class LoginFailureHandler implements AuthenticationFailureHandler {
 
     response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
     response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
+
+    ErrorResponse errorResponse = new ErrorResponse(
+        HttpServletResponse.SC_UNAUTHORIZED,
+        exception.getClass().getSimpleName(),
+        "UNAUTHORIZED",
+        exception.getMessage(),
+        Map.of("path",
+            request.getRequestURI())
+    );
 
     ObjectMapper objectMapper = new ObjectMapper();
     String errorJson = objectMapper.writeValueAsString(
-        Map.of("message", exception.getMessage())
+        errorResponse
     );
     response.getWriter().write(errorJson);
 
